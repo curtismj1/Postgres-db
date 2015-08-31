f2e0df4330e342fee7643de6605451498a5c77df
curtismj1 <curtismj1@gcc.edu>
2015-08-31 16:27:15 -0400

    stuff

---
+++ PostGres Database/PostGres Database/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PostGres_Database.MvcApplication" Language="C#" %>
---
+++ PostGres Database/PostGres Database/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PostGres_Database.Startup))]
+namespace PostGres_Database
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
